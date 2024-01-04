7a5c8c9e199c1503713ed61486de6637bf0a09f8
Site5st10 <1rexvero1@gmail.com>
2024-01-04 20:46:46 +0330

    bootstrap, scss for css, font, images & js

---
+++ assets/js/card.js
@@ -0,0 +1,31 @@
+const body = document.getElementById('body');
+const navbar = document.getElementById('navbar');
+const sun = document.getElementById('sun');
+const moon = document.getElementById('moon');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const img = document.getElementById('img');
+sun.addEventListener('click', () => {
+    sun.classList.add('d-none');
+    moon.classList.remove('d-none');
+    body.classList.add('bg');
+    navbar.classList.remove('bg-2');
+    navbar.classList.add('bg-3');
+    prev.classList.add('text-2');
+    next.classList.add('text-2');
+    prev.classList.remove('text');
+    next.classList.remove('text');
+});
+moon.addEventListener('click', () => {
+    sun.classList.remove('d-none');
+    moon.classList.add('d-none');
+    body.classList.remove('bg');
+    body.classList.add('trans');
+    navbar.classList.add('bg-2');
+    navbar.classList.remove('bg-3');
+    navbar.classList.add('trans');
+    prev.classList.add('text');
+    next.classList.add('text');
+    prev.classList.remove('text-2');
+    next.classList.remove('text-2');
+});
